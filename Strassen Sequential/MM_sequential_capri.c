cc22fad44b98e4ff3974f1e3e364bc5131c1e124
nico9779 <nicola.levorato.2@studenti.unipd.it>
2021-11-15 15:44:54 +0100

    Fixed code for testing MM in capri

--- Strassen Sequential/MM_sequential_capri.c
+++ Strassen Sequential/MM_sequential_capri.c
@@ -4,7 +4,28 @@
 #include <time.h>
 #include <xmmintrin.h>
 
-void multiplyMatrix(float *A, float *B, float *C, int n) {
+void multiplyMatrixIKJ(float* A, float* B, float* C, int n) {
+
+    float a = 0;
+
+	for(int i = 0; i < n; i++) {
+		for(int k = 0; k < n; k++) {
+            a = A[i*n+k];
+			for(int j = 0; j < n; j+=8) {
+                C[i*n+j] += a * B[k*n+j];
+                C[i*n+j+1] += a * B[k*n+j+1];
+                C[i*n+j+2] += a * B[k*n+j+2];
+                C[i*n+j+3] += a * B[k*n+j+3];
+                C[i*n+j+4] += a * B[k*n+j+4];
+                C[i*n+j+5] += a * B[k*n+j+5];
+                C[i*n+j+6] += a * B[k*n+j+6];
+                C[i*n+j+7] += a * B[k*n+j+7];
+			}
+		}
+	}
+}
+
+void multiplyMatrixSSE(float *A, float *B, float *C, int n) {
     __m128 ma , *mb , *mc = ( __m128 *) C;
     int t1 ,t2;
     int n_4 = (n>>3)<<1;
@@ -14,13 +35,25 @@ void multiplyMatrix(float *A, float *B, float *C, int n) {
             ma = _mm_load1_ps(A++); //k*a+i
             for (int j=0; j<n_4; j+=4) {
                 mc[j] = _mm_add_ps (mc[j], _mm_mul_ps (ma, mb[j]));
-                mc[j+1] = _mm_add_ps (mc[j+1] , _mm_mul_ps (ma, mb[j+1]));
-                mc[j+2] = _mm_add_ps (mc[j+2] , _mm_mul_ps (ma, mb[j+2]));
-                mc[j+3] = _mm_add_ps (mc[j+3] , _mm_mul_ps (ma, mb[j+3]));
+                mc[j+1] = _mm_add_ps (mc[j+1], _mm_mul_ps (ma, mb[j+1]));
+                mc[j+2] = _mm_add_ps (mc[j+2], _mm_mul_ps (ma, mb[j+2]));
+                mc[j+3] = _mm_add_ps (mc[j+3], _mm_mul_ps (ma, mb[j+3]));
             }
         }
         mc += n_4 ;
     }
+}
+
+void printMatrix(float* M, int n) {
+
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n; j++) {
+			printf("%f  ", M[i*n+j]);
+		}
+		printf("\n");
+	}
+
+	printf("\n");
 }
 
 int main(int argc, char **argv) {
@@ -34,11 +67,12 @@ int main(int argc, char **argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     if(rank == 0) {
-        float *A, *B, *C;
+        float *A, *B, *C, *D;
 
         A = (float*) malloc(n * n * sizeof(float));
         B = (float*) malloc(n * n * sizeof(float));
-        C = (float*) malloc(n * n * sizeof(float));
+        C = (float*) calloc(n * n, sizeof(float));
+        D = (float*) calloc(n * n, sizeof(float));
 
         srand(time(NULL));
 
@@ -50,13 +84,24 @@ int main(int argc, char **argv) {
         }
 
         start = MPI_Wtime();
-        multiplyMatrix(A, B, C, n);
+        multiplyMatrixIKJ(A, B, C, n);
         end = MPI_Wtime();
+
+        printf("Time took sequential MM IKJ: %f ms\n", (end-start)*1000);
+
+        start = MPI_Wtime();
+        multiplyMatrixSSE(A, B, D, n);
+        end = MPI_Wtime();
+
+        printf("Time took sequential MM with SSE: %f ms\n", (end-start)*1000);
 
         free(A);
         free(B);
         free(C);
-
-        printf("Time took sequential MM: %f ms\n", (end-start)*1000);
+        free(D);
     }
+
+    MPI_Finalize();
+
+    return 0;
 }
