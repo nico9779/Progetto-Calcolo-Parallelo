6074cd289e3835a5d4f27a846108cb69f1d77dce
nico9779 <nicola.levorato.2@studenti.unipd.it>
2021-11-26 18:16:26 +0100

    Optimized parallel MM

--- Strassen Parallel/Matrix_multiplication_parallel.c
+++ Strassen Parallel/Matrix_multiplication_parallel.c
@@ -1,13 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <mpi.h>
 
-// Print matrix in output
-void printMatrix(int* M, int rows, int cols) {
+#define CHUNK 32
 
-	for(int i=0; i<rows; i++) {
-		for(int j=0; j<cols; j++) {
-			printf("%d ", M[i*cols+j]);
+double average(double times[], int size) {
+
+    double average = 0;
+
+    for(int i=0; i<size; i++) {
+        average += times[i];
+    }
+
+    average = average/size;
+
+    return average;
+}
+
+// Print matrix in output
+void printMatrix(float* M, int n) {
+
+	for(int i=0; i<n; i++) {
+		for(int j=0; j<n; j++) {
+			printf("%f  ", M[i*n+j]);
 		}
 		printf("\n");
 	}
@@ -15,26 +31,11 @@ void printMatrix(int* M, int rows, int cols) {
 	printf("\n");
 }
 
-// Allocate memory to store square matrix
-int* allocateSquareMatrixMemory(int n) {
-
-    int* temp = (int*) malloc(n * n * sizeof(int));
-
-    return temp;
-}
-
-// Allocate memory to store matrix
-int* allocateMatrixMemory(int rows, int cols) {
-
-    int* temp = (int*) malloc(rows * cols * sizeof(int));
-
-    return temp;
-}
 
 // Add two square matrices
-int* addMatrix(int* M1, int* M2, int n) {
+float* addMatrix(float* M1, float* M2, int n) {
 
-	int* temp = allocateSquareMatrixMemory(n);
+	float* temp = (float*) malloc(n * n * sizeof(float));
 
 	for(int i=0; i<n; i++) {
 		for(int j=0; j<n; j++) {
@@ -47,9 +48,9 @@ int* addMatrix(int* M1, int* M2, int n) {
 }
 
 // Subtract two square matrices
-int* subtractMatrix(int* M1, int* M2, int n) {
+float* subtractMatrix(float* M1, float* M2, int n) {
 
-	int* temp = allocateSquareMatrixMemory(n);
+	float* temp = (float*) malloc(n * n * sizeof(float));
 
     for(int i=0; i<n; i++) {
 		for(int j=0; j<n; j++) {
@@ -61,47 +62,77 @@ int* subtractMatrix(int* M1, int* M2, int n) {
     return temp;
 }
 
-// Multiply two matrices using standard definition
-int* multiplyMatrix(int* A, int* B, int rA, int cA, int cB) {
+// Multiply two matrices sequentially using standard definition
+float* multiplyMatrixSequential(float* A, float* B, int n) {
 
-	int* C = allocateMatrixMemory(rA, cB);
+	float* C = (float*) calloc(n * n, sizeof(float));
+    float a = 0;
 
-	for(int i = 0; i < rA; i++) {
-		for(int j = 0; j < cB; j++) {
-			int index = i*cB+j;
-            C[index] = A[i*cA] * B[j];
-			for(int k = 1; k < cA; k++) {
-				C[index] += A[i*cA+k] * B[k*cB+j];
-			}
-		}
-	}
-
+    for(int kk=0; kk<n; kk+=CHUNK) {
+        for(int i=0; i<n; ++i) {
+            for(int k=kk; k<kk+CHUNK; ++k) {
+                a = A[i*n+k];
+                for(int j=0; j<n; j+=8) {
+                    C[i*n+j] += a * B[k*n+j];
+                    C[i*n+j+1] += a * B[k*n+j+1];
+                    C[i*n+j+2] += a * B[k*n+j+2];
+                    C[i*n+j+3] += a * B[k*n+j+3];
+                    C[i*n+j+4] += a * B[k*n+j+4];
+                    C[i*n+j+5] += a * B[k*n+j+5];
+                    C[i*n+j+6] += a * B[k*n+j+6];
+                    C[i*n+j+7] += a * B[k*n+j+7];
+                }
+            }
+	    }
+    }
+	
 	return C;
 }
 
 // Multiply two matrices using Strassen algorithm
-int* strassenMatrix(int* A, int* B, int n) {
+float* strassenMatrix(float* A, float* B, int n) {
 
 	// Base case
-	if(n <= 64) {
-    	return multiplyMatrix(A, B, n, n, n);
+	if(n <= 256) {
+        float* C = (float*) calloc(n * n, sizeof(float));
+    	float a = 0;
+
+        for(int kk=0; kk<n; kk+=CHUNK) {
+            for(int i=0; i<n; ++i) {
+                for(int k=kk; k<kk+CHUNK; ++k) {
+                    a = A[i*n+k];
+                    for(int j=0; j<n; j+=8) {
+                        C[i*n+j] += a * B[k*n+j];
+                        C[i*n+j+1] += a * B[k*n+j+1];
+                        C[i*n+j+2] += a * B[k*n+j+2];
+                        C[i*n+j+3] += a * B[k*n+j+3];
+                        C[i*n+j+4] += a * B[k*n+j+4];
+                        C[i*n+j+5] += a * B[k*n+j+5];
+                        C[i*n+j+6] += a * B[k*n+j+6];
+                        C[i*n+j+7] += a * B[k*n+j+7];
+                    }
+                }
+            }
+        }
+
+        return C;
 	}
 
 	// Initialize matrix C to return in output (C = A*B)
-	int* C = allocateSquareMatrixMemory(n);
+	float* C = (float*) malloc(n * n * sizeof(float));
 
 	// Dimension of the matrices is the half of the input size
 	int k = n/2;
 
 	// Decompose A and B into 8 submatrices
-	int* A11 = allocateSquareMatrixMemory(k);
-	int* A12 = allocateSquareMatrixMemory(k);
-	int* A21 = allocateSquareMatrixMemory(k);
-	int* A22 = allocateSquareMatrixMemory(k);
-	int* B11 = allocateSquareMatrixMemory(k);
-	int* B12 = allocateSquareMatrixMemory(k);
-	int* B21 = allocateSquareMatrixMemory(k);
-	int* B22 = allocateSquareMatrixMemory(k);
+	float* A11 = (float*) malloc(k * k * sizeof(float));
+	float* A12 = (float*) malloc(k * k * sizeof(float));
+	float* A21 = (float*) malloc(k * k * sizeof(float));
+	float* A22 = (float*) malloc(k * k * sizeof(float));
+	float* B11 = (float*) malloc(k * k * sizeof(float));
+	float* B12 = (float*) malloc(k * k * sizeof(float));
+	float* B21 = (float*) malloc(k * k * sizeof(float));
+	float* B22 = (float*) malloc(k * k * sizeof(float));
 
 	for(int i=0; i<k; i++) {
 		for(int j=0; j<k; j++) {
@@ -122,25 +153,25 @@ int* strassenMatrix(int* A, int* B, int n) {
 	}
 
 	// Create support matrices in order to calculate Strassen matrices
-	int* M1 = subtractMatrix(B12, B22, k);
-	int* M2 = addMatrix(A11, A12, k);
-	int* M3 = addMatrix(A21, A22, k);
-	int* M4 = subtractMatrix(B21, B11, k);
-	int* M5 = addMatrix(A11, A22, k);
-	int* M6 = addMatrix(B11, B22, k);
-	int* M7 = subtractMatrix(A12, A22, k);
-	int* M8 = addMatrix(B21, B22, k);
-	int* M9 = subtractMatrix(A11, A21, k);
-	int* M10 = addMatrix(B11, B12, k);
+	float* M1 = subtractMatrix(B12, B22, k);
+	float* M2 = addMatrix(A11, A12, k);
+	float* M3 = addMatrix(A21, A22, k);
+	float* M4 = subtractMatrix(B21, B11, k);
+	float* M5 = addMatrix(A11, A22, k);
+	float* M6 = addMatrix(B11, B22, k);
+	float* M7 = subtractMatrix(A12, A22, k);
+	float* M8 = addMatrix(B21, B22, k);
+	float* M9 = subtractMatrix(A11, A21, k);
+	float* M10 = addMatrix(B11, B12, k);
 
 	// Create the Strassen matrices
-	int* P1 = strassenMatrix(A11, M1, k);
-	int* P2 = strassenMatrix(M2, B22, k);
-	int* P3 = strassenMatrix(M3, B11, k);
-	int* P4 = strassenMatrix(A22, M4, k);
-	int* P5 = strassenMatrix(M5, M6, k);
-	int* P6 = strassenMatrix(M7, M8, k);
-	int* P7 = strassenMatrix(M9, M10, k);
+	float* P1 = strassenMatrix(A11, M1, k);
+	float* P2 = strassenMatrix(M2, B22, k);
+	float* P3 = strassenMatrix(M3, B11, k);
+	float* P4 = strassenMatrix(A22, M4, k);
+	float* P5 = strassenMatrix(M5, M6, k);
+	float* P6 = strassenMatrix(M7, M8, k);
+	float* P7 = strassenMatrix(M9, M10, k);
 
 	free(A11);
     free(A12);
@@ -162,16 +193,16 @@ int* strassenMatrix(int* A, int* B, int n) {
 	free(M9);
 	free(M10);
 
-	int* M11 = addMatrix(P5, P4, k);
-	int* M12 = addMatrix(M11, P6, k);
-	int* M13 = addMatrix(P5, P1, k);
-	int* M14 = subtractMatrix(M13, P3, k);
+	float* M11 = addMatrix(P5, P4, k);
+	float* M12 = addMatrix(M11, P6, k);
+	float* M13 = addMatrix(P5, P1, k);
+	float* M14 = subtractMatrix(M13, P3, k);
 
 	// Compose matrix C from the submatrices
-	int* C11 = subtractMatrix(M12, P2, k);
-	int* C12 = addMatrix(P1, P2, k);
-	int* C21 = addMatrix(P3, P4, k);
-	int* C22 = subtractMatrix(M14, P7, k);
+	float* C11 = subtractMatrix(M12, P2, k);
+	float* C12 = addMatrix(P1, P2, k);
+	float* C21 = addMatrix(P3, P4, k);
+	float* C22 = subtractMatrix(M14, P7, k);
 
     free(P1);
 	free(P2);
@@ -206,66 +237,116 @@ int* strassenMatrix(int* A, int* B, int n) {
 
 int main(int argc, char **argv) {
 
-    const int n = 1024;
+    const int n = 4096;
+	const int repetition = 5;
 
     int rank, size;
     double start, end;
+	double times[repetition];
 
-    int *A, *B, *C;
-    A = (int*) malloc(n * n * sizeof(int));
-    B = (int*) malloc(n * n * sizeof(int));
-    C = (int*) malloc(n * n * sizeof(int));
+    float *A, *B, *C;
+	B = (float*) malloc(n * n * sizeof(float));
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+	const int num_elements = n*n/size;
+
     if(rank == 0) {
 
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
-                A[i*n+j] = i+j;
-                B[i*n+j] = i-j;
+		A = (float*) malloc(n * n * sizeof(float));
+
+        srand(time(NULL));
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                A[i*n+j] = rand()%10+1;
+                B[i*n+j] = rand()%10+1;
             }
         }
+	}
 
-        start = MPI_Wtime();
-        strassenMatrix(A, B, n);
-        end = MPI_Wtime();
+	//***** SEQUENTIAL PHASE *****//
 
-        printf("Time took strassen: %f ms\n", (end-start)*1000);
+	if(rank == 0) {
+
+        for(int i=0; i<repetition; i++) {
+
+            // Calculate time to run Strassen sequential algorithm
+            start = MPI_Wtime();
+            C = strassenMatrix(A, B, n);
+            end = MPI_Wtime();
+
+            times[i] = (end-start)*1000;
+
+            free(C);
+        }
+
+        printf("Average time took sequential Strassen: %f ms\n", average(times, repetition));
     }
 
-    MPI_Barrier(MPI_COMM_WORLD);
-    start = MPI_Wtime();
+	//***** PARALLEL PHASE *****//
 
-    MPI_Scatter(A, n*n/size, MPI_INT, A, n*n/size, MPI_INT, 0, MPI_COMM_WORLD);
+	for(int i=0; i<repetition; i++) {
 
-    MPI_Bcast(B, n*n, MPI_INT, 0, MPI_COMM_WORLD);
+		if(rank == 0) {
+			C = (float*) malloc(n * n * sizeof(float));
+		}
+
+		start = MPI_Wtime();
+
+		float* local_A = (float*) malloc(num_elements * sizeof(float));
+    	float* local_C = (float*) calloc(num_elements, sizeof(float));
+
+		MPI_Scatter(A, num_elements, MPI_FLOAT, local_A, num_elements, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+		MPI_Bcast(B, n*n, MPI_FLOAT, 0, MPI_COMM_WORLD);
+		
+		float a = 0;
+
+		for(int kk=0; kk<n; kk+=CHUNK) {
+			for(int i=0; i<n/size; ++i) {
+				for(int k=kk; k<kk+CHUNK; ++k) {
+					a = local_A[i*n+k];
+					for(int j=0; j<n; j+=8) {
+						local_C[i*n+j] += a * B[k*n+j];
+						local_C[i*n+j+1] += a * B[k*n+j+1];
+						local_C[i*n+j+2] += a * B[k*n+j+2];
+						local_C[i*n+j+3] += a * B[k*n+j+3];
+						local_C[i*n+j+4] += a * B[k*n+j+4];
+						local_C[i*n+j+5] += a * B[k*n+j+5];
+						local_C[i*n+j+6] += a * B[k*n+j+6];
+						local_C[i*n+j+7] += a * B[k*n+j+7];
+					}
+				}
+			}
+		}
+
+		free(local_A);
+
+		MPI_Gather(local_C, num_elements, MPI_FLOAT, C, num_elements, MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+		free(local_C);
+		
+		end = MPI_Wtime();
+
+		if(rank == 0) {
+			//printMatrix(C, n);
+			free(C);
+		}
+
+		times[i] = (end-start)*1000;
+	}
     
-    for(int i=0; i<n/size; i++) {
-        for(int j=0; j<n; j++) {
-            int index = i*n+j;
-            C[index] = A[i*n] * B[j];
-            for(int k=1; k<n; k++) {
-                C[index] += A[i*n+k] * B[k*n+j];
-            }
-        }
-    }
-
-    MPI_Gather(C, n*n/size, MPI_INT, C, n*n/size, MPI_INT, 0, MPI_COMM_WORLD);
-
-    free(A);
-    free(B);
-    free(C);
-
-    MPI_Barrier(MPI_COMM_WORLD);
-    end = MPI_Wtime();
     MPI_Finalize();
 
     if(rank == 0) {
-        printf("Time took parallel MM: %f ms\n", (end-start)*1000);
+		free(A);
+        printf("Average time took parallel Strassen: %f ms\n", average(times, repetition));
     }
+
+	free(B);
 
     return 0;
 }
